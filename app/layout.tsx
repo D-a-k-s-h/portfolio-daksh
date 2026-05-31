1717b93c05020ed88972984af1b76d3fb40da94e
consint-daksh <daksh.kumar@consint.ai>
2026-05-31 18:39:49 +0530

    Resolved build failure

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="dark"
           enableSystem
-          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
